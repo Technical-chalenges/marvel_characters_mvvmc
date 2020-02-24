cb0128b00b4d9a703ab78ee3761508e0a39a63ad
Alexandr <tsfadeev@yandex.ru>
2020-02-24 22:00:37 +0300

    Удален сториборд

--- MVVMCMarvelCharacters/App/SceneDelegate.swift
+++ MVVMCMarvelCharacters/App/SceneDelegate.swift
@@ -1,10 +1,8 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
     var appCoordinator: AppCoordinator!
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
